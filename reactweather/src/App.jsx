096b54b3e5fd696e6e68442c31411e5797bba984
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2024-03-03 22:25:59 +0530

    first commit

---
+++ reactweather/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Weatherapp from './components/weatherapp/Weatherapp'
+
+function App() {
+  
+  return (
+    <div>
+      <Weatherapp/>
+    </div>
+  )
+}
+
+export default App
